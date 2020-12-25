274929af805b54e44e45442a439d5076bd42cddf
aetrnm <oleggnatkovskii@gmail.com>
2020-12-25 22:17:43 +0200

    first commit

---
+++ Serialization/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public struct Contact
+    {
+        public string Name;
+        public string Number;
+
+        public override string ToString()
+        {
+            return $"{Name} +{Number}";
+        }
+    }
+
+    public static class ContactHelper
+    {
+        public static Contact? ContactParse(this string input)
+        {
+            var data = input.Split(' ');
+
+            if (data.Length < 2)
+                return null;
+
+            Contact contact = new Contact();
+            contact.Name = data[0];
+            contact.Number = data[1];
+            return contact;
+        }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace Serialization
+{
+    class Program
+    {
+        private static List<Contact> contacts;
+
+        static void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        const string Path = "PhoneBook.txt";
+        const string PathXML = "PhoneBookXML.txt";
+
+        /*static void Save()
+        {
+            using (StreamWriter sw = new StreamWriter(Path))
+            {
+                foreach (Contact c in contacts)
+                {
+                    sw.WriteLine(c);
+                }
+            }
+        }*/
+
+        static void SaveToXml()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(List<Contact>));
+            using (StreamWriter sw = new StreamWriter(PathXML))
+            {
+                xmlSerializer.Serialize(sw, contacts);
+            }
+        }
+
+        static List<Contact> GetFromXml()
+        {
+            var xmlDeserializer = new XmlSerializer(typeof(List<Contact>));
+            using (StreamReader sr = new StreamReader(PathXML))
+            {
+                var contacts = xmlDeserializer.Deserialize(sr);
+            }
+            return contacts;
+        }
+
+        static void Main(string[] args)
+        {
+            contacts = new List<Contact>();
+
+            Contact IvanPetrovich;
+            IvanPetrovich.Name = "Ivan Petrovich";
+            IvanPetrovich.Number = "38028312381";
+            AddContact(IvanPetrovich);
+            SaveToXml();
+            var contactsFromFile = GetFromXml();
+            foreach (var VARIABLE in contactsFromFile)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
